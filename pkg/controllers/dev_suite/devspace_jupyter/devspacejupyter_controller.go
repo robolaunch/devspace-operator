970b21d8337ae35ddb820fa9374ebace958b799b
tunahanertekin <tunahan.ertekin13@gmail.com>
2023-06-16 14:26:51 +0300

    refactor(jupyter): add default controller functions

--- pkg/controllers/dev_suite/devspace_jupyter/devspacejupyter_controller.go
+++ pkg/controllers/dev_suite/devspace_jupyter/devspacejupyter_controller.go
@@ -19,11 +19,13 @@ package devspace_jupyter
 import (
 	"context"
 
+	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 
+	"github.com/go-logr/logr"
 	devv1alpha1 "github.com/robolaunch/devspace-operator/pkg/api/roboscale.io/v1alpha1"
 )
 
@@ -37,9 +39,56 @@ type DevSpaceJupyterReconciler struct {
 //+kubebuilder:rbac:groups=dev.roboscale.io,resources=devspacejupyters/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=dev.roboscale.io,resources=devspacejupyters/finalizers,verbs=update
 
+//+kubebuilder:rbac:groups=core,resources=pods,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=core,resources=services,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=networking.k8s.io,resources=ingresses,verbs=get;list;watch;create;update;patch;delete
+
+var logger logr.Logger
+
 func (r *DevSpaceJupyterReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+	logger = log.FromContext(ctx)
+
+	instance, err := r.reconcileGetInstance(ctx, req.NamespacedName)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			return ctrl.Result{}, nil
+		}
+		return ctrl.Result{}, err
+	}
+
+	if !instance.DeletionTimestamp.IsZero() {
+		return ctrl.Result{}, nil
+	}
+
+	err = r.reconcileCheckStatus(ctx, instance)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	err = r.reconcileUpdateInstanceStatus(ctx, instance)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	err = r.reconcileCheckResources(ctx, instance)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	err = r.reconcileUpdateInstanceStatus(ctx, instance)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
 	return ctrl.Result{}, nil
+}
+
+func (r *DevSpaceJupyterReconciler) reconcileCheckStatus(ctx context.Context, instance *devv1alpha1.DevSpaceJupyter) error {
+	return nil
+}
+
+func (r *DevSpaceJupyterReconciler) reconcileCheckResources(ctx context.Context, instance *devv1alpha1.DevSpaceJupyter) error {
+	return nil
 }
 
 // SetupWithManager sets up the controller with the Manager.
--- pkg/controllers/dev_suite/devspace_jupyter/helpers.go
+++ pkg/controllers/dev_suite/devspace_jupyter/helpers.go
@@ -1,1 +1,50 @@
 package devspace_jupyter
+
+import (
+	"context"
+
+	"github.com/robolaunch/devspace-operator/internal/label"
+	devv1alpha1 "github.com/robolaunch/devspace-operator/pkg/api/roboscale.io/v1alpha1"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/util/retry"
+)
+
+func (r *DevSpaceJupyterReconciler) reconcileGetInstance(ctx context.Context, meta types.NamespacedName) (*devv1alpha1.DevSpaceJupyter, error) {
+	instance := &devv1alpha1.DevSpaceJupyter{}
+	err := r.Get(ctx, meta, instance)
+	if err != nil {
+		return &devv1alpha1.DevSpaceJupyter{}, err
+	}
+
+	return instance, nil
+}
+
+func (r *DevSpaceJupyterReconciler) reconcileUpdateInstanceStatus(ctx context.Context, instance *devv1alpha1.DevSpaceJupyter) error {
+	return retry.RetryOnConflict(retry.DefaultRetry, func() error {
+		instanceLV := &devv1alpha1.DevSpaceJupyter{}
+		err := r.Get(ctx, types.NamespacedName{
+			Name:      instance.Name,
+			Namespace: instance.Namespace,
+		}, instanceLV)
+
+		if err == nil {
+			instance.ResourceVersion = instanceLV.ResourceVersion
+		}
+
+		err1 := r.Status().Update(ctx, instance)
+		return err1
+	})
+}
+
+func (r *DevSpaceJupyterReconciler) reconcileGetTargetDevSpace(ctx context.Context, instance *devv1alpha1.DevSpaceJupyter) (*devv1alpha1.DevSpace, error) {
+	devspace := &devv1alpha1.DevSpace{}
+	err := r.Get(ctx, types.NamespacedName{
+		Namespace: instance.Namespace,
+		Name:      label.GetTargetDevSpace(instance),
+	}, devspace)
+	if err != nil {
+		return nil, err
+	}
+
+	return devspace, nil
+}
